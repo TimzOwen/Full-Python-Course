be13daf00db8b47f6aef5eb56cc3db52684eac6c
Timz Owen <ktimothy@kabarak.ac.ke>
2021-01-31 13:24:31 +0300

    Update Lesson 1 modules.py
    modules update

--- chapter four/Lesson 1 modules.py
+++ chapter four/Lesson 1 modules.py
@@ -1,1 +1,20 @@
 
+
+## Python Modules
+# Modules helps us organize code in multiple Files
+# Each File is a module  
+# create a new File / modelu to import from
+# make sure Files are in the same folder 
+# check the Modules Folder to upload
+
+
+
+
+  
+
+
+
+
+# Challenge 
+# create a numbers iteration loop to find the largest
+# make sure to have sepate modules
