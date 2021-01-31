20227787cfbe2a82381b7b6cfd86e27ffe6abddb
Timz Owen <ktimothy@kabarak.ac.ke>
2021-01-31 13:28:52 +0300

    Create shipping.py
    shipping

---
+++ chapter four/modules_PackgeFile/shipping.py
@@ -0,0 +1,9 @@
+# Function to calculate price
+
+def cal_shipping():
+    print("Calculate shipping cost for the product")
+
+
+# function to collect address
+def customer_address():
+    print("what is your physical address ?")
