003980359dc9d6ec03dd96f9fcd47027c237ddc2
Timz Owen <ktimothy@kabarak.ac.ke>
2021-01-31 13:26:18 +0300

    Update Lesson 1 modules.py
    sample update for packages and modules

--- chapter four/Lesson 1 modules.py
+++ chapter four/Lesson 1 modules.py
@@ -9,8 +9,43 @@
 
 
 
+# import specific modules
+from ecommerce.shipping import cal_shipping
 
-  
+cal_shipping() # now the method gets called.
+
+
+# import all functions in a package
+from ecommerce import shipping
+shipping.customer_address()
+shipping.cal_shipping()
+
+# Central bank package fow cash payments and transactions
+from banking import withdrawals
+withdrawals.transactions(50000)
+withdrawals.monthly_withdrawals(120000)
+
+
+
+# check if path exists in a direcrtory
+from pathlib import Path
+
+path = Path() # the current path
+
+path5 = Path("checkPath")
+print(path5.exists()) # Returns a boolean
+
+# create a new directory
+path2 = Path("MainServer")
+# print(path2.mkdir()) # creates a new directory
+
+path3 = Path("MainServer")
+print(path3.exists()) # Returns True
+# path3.rmdir()
+# print(path3.exists()) # returns False
+
+for files in path.glob('*'):
+    print(files) # prints all Files in the iteration
 
 
 
