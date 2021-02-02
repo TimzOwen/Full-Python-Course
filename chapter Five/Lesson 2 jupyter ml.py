03d6818a746a35f7b22a565d044c05bb7bbcb971
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-02 11:07:00 +0300

    Create Lesson 2 jupyter ml.py
    using jupyter notebook

---
+++ chapter Five/Lesson 2 jupyter ml.py
@@ -0,0 +1,30 @@
+  # N/B: 
+  # CHECK THE MACHINE LEARNING FOLDER FOR THE PRACTICE
+
+
+# Lesson 2.
+# Working with datasets
+
+# create a note in jupyter notebook and do some test before importing the data
+
+print("Ml test/ Hello world ml")
+
+# Get a dataset from Kaggle.com to use in the project
+
+
+## NA
+# Nagivating jupyter
+'''
+    quick navigation in juypter (Keyboard shortcut)
+    esc--> Enter command mode
+    A -> adds a cell above
+    B -> adds a cell bellow
+    D * 2 --> deletes a cell
+    
+    cursor on a method + tab -> shows all methods that can be performed
+    shift + tab --> shows the description of a method
+'''
+
+# challenge
+## simulates realworld project 
+ 
