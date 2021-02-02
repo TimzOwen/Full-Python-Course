cb05ad0379838ffbbda636f55e86dc3d03c42332
Timz Owen <ktimothy@kabarak.ac.ke>
2021-02-02 08:52:44 +0300

    Update Lesson 1 Machine Learning.py
    introduction to machine learning

--- chapter Five/Lesson 1 Machine Learning.py
+++ chapter Five/Lesson 1 Machine Learning.py
@@ -1,1 +1,29 @@
 
+# Chapter 5
+# Lesson 1
+
+# Machine Learning with Python
+# IDE --> Jupyter lab (For this course. most recomended)
+'''
+    Steps in machine learning :
+    1. import the data
+    2. Clean the data 
+    3. split data into 
+        Training 
+        Testing
+    4. Create model
+    5. Train the model
+    6. Make predictions
+'''
+
+'''
+    Machine Learning libraries:
+    1. Numpy--> Multidimentional array library
+    2. Pandas--> Data analysis library (Data frame--2D frame like spreadsheet)
+    3. matplotlib --> 2-D plotting library for creating graphs and plots
+    4. scikilearn--> provide most algorithim like:
+                        decision trees
+                        Neural network
+'''
+
+# will be working with jupyter notebook:
