5b6bae059cb80fc3a7a9039be8cc0e3c6d82f6be
Timz Owen <ktimothy@kabarak.ac.ke>
2021-03-20 22:43:52 +0300

    Update project 7 odering.py
    soln 2 update

--- Projects/Beginners/project 7 odering.py
+++ Projects/Beginners/project 7 odering.py
@@ -15,3 +15,16 @@ def count_evens(numbersList):
     return len(count)
 print(count_evens([2,4,6,8]))
 print(count_evens([1,2,3,4,5,6,7,8,9,10]))
+
+
+# soln 2
+
+def count_evens(numbers):
+    counter = 0;
+    for nums in numbers:
+        if nums%2 == 0:
+            counter += 1
+    return counter
+print(count_evens([2,4,6,8,10]))
+
+
