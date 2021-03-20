21ae5121e1ea102d0bb52cc82692c37f6916d75d
Timz Owen <ktimothy@kabarak.ac.ke>
2021-03-20 22:45:02 +0300

    Update project 8 computer guess.py
    computer turn guess

--- Projects/Beginners/project 8 computer guess.py
+++ Projects/Beginners/project 8 computer guess.py
@@ -30,3 +30,27 @@ def guess_game(num):
     print(f"you just guesed {random_num} =correctly")
 print(guess_game(15))
 
+
+
+
+# computer Guess Game ---> time for computer to guess the number
+# CREATE A COMPUTER GUESS GAME:
+ 
+import random
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low   # or even low
+        feedback = input(f"Is {guess} too high (H) or too low (L) or correct (C) ??").lower()
+        if feedback == 'h':
+            high = guess - 1 
+        elif feedback == 'l':
+            low = guess + 1
+    print(f"The Computer gussed it right !!! its {guess}")
+computer_guess(100)
