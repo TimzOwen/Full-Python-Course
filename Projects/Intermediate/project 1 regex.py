699c2831b69a13421d6ab83da53a8fd4eec2d0a3
Timz Owen <ktimothy@kabarak.ac.ke>
2021-03-22 12:46:30 +0300

    Update project 1 regex.py
    solution in the version of regex in python

--- Projects/Intermediate/project 1 regex.py
+++ Projects/Intermediate/project 1 regex.py
@@ -2,6 +2,10 @@
 # Regular Expression 
 # Regular expression makes your job easier especially when searching for specific patterns
 
+
+# QUIZ NUMBER 1
+
+
 # Example: Phone number validation
 #(415-555-4242) ---> Kenya number formart
 
@@ -35,3 +39,19 @@ print('Done')
 # search for phone number in a text:
 print(isPhoneNumber(message))
 
+
+# solution in Regex
+
+
+# Done in regular Expression
+# sample phone number text search :
+import re
+
+phone_number = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phone_number.search('my number is 415-255-4040 for official use')
+print("phone number found: " + mo.group())
+
+# reduce the compile arguements 
+kenya_phon = re.compile(r'\d{3}-\d{3}-\d{6}')
+phone = kenya_phon.search("This is the phone number 254-740-354167 to call for help")
+print("Help number: " + phone.group())
