87f1ab23e67f2ee1ce2703407c4baf2b9efdd1a3
Timz Owen <ktimothy@kabarak.ac.ke>
2021-09-17 12:05:20 +0300

    updated intro 2

---
+++ chapter one/001_introductionTwo.py
@@ -0,0 +1,49 @@
+## Python basics 
+
+
+## print output to the console
+print("Hello Timz Owen")
+print(100)
+print('I am a python developer')
+
+
+## Values and Data types
+
+## int--->intergers ---->1,2,3,4,5,6....
+## str--->String values---->"appears idn doubl e "
+## float-->decimal number value--->2.5, 5.70, 59.8, 80.50,
+
+print("float ", 3.5)
+print("int",8)
+print("string" , "Timz")
+
+
+## operators and operands
+
+# addition
+print(50 + 50)
+
+# subtraction
+print(50 - 10)
+
+# multiplication 
+print(8 * 8)
+
+# Division
+print(8/4)
+
+# tran
+print(3//2) # trangets the results leaving the whole numbers
+
+# modulous oper
+print(7%2) # prints the remainder of a number
+
+# exponential 
+print(3**2)
+
+
+
+## order of operation
+## BODMAS
+print(2+2*2/2-2)
+print((2/2) + 2 * (2-3) )
