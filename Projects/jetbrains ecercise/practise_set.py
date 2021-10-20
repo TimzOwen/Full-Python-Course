0e2451ee374caa0e553a49d87e2f87f15e770ba7
TimzOwen <ktimothy@kabarak.ac.ke>
2021-10-20 15:26:10 +0300

    updated quiz 1

---
+++ Projects/jetbrains ecercise/practise_set.py
@@ -0,0 +1,9 @@
+
+# get flight cost, number of passengers and days for a vacation ang get total cost 
+
+# put your python code here
+days = int(input("Enter number of days "))
+food = int(input("How many days "))
+flight = int(input("Total flight"))
+
+print((days * food *flight))
